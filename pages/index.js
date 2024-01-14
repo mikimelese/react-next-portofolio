02c4a695ad19b6b6780044bd1170d7b10be9caac
mikyas melese <mikimelese@gmail.com>
2024-01-14 17:00:58 +0300

    remove padding for mobile

--- pages/index.js
+++ pages/index.js
@@ -181,7 +181,7 @@ export default function Home() {
               </li>
             </ul>
           </nav>
-          <div className="text-center p-10 py-10">
+          <div className="text-center  sm:p-10 py-10">
             <h2 className="text-5xl py-2 text-teal-600 font-medium dark:text-teal-400 md:text-6xl">
               Mikyas Melese
             </h2>
